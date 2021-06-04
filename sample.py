a1c92592bce0ac28b17621324874e77b5adf4124
unknown <adetutuadejuyigbe@gmail.com>
2021-06-05 01:23:38 +0200

    myfile commit

---
+++ sample.py
@@ -0,0 +1,15 @@
+creator = "Tutu"
+
+PI = 3.14159
+
+def Area(radius):
+    PI = 3.14159
+    
+    return PI * radius ** 2 
+
+
+def per_of_tri(a,b,c):
+    perimeter = a + b + c
+    
+    return perimeter
+     
